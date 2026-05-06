f666fae229d1b8cd4dc025954bd99ae88eca198c
Михайлов Михаил <mmikhaylov@tekora.ru>
2026-05-07 00:24:20 +0400

    Добавлено определение игрока для первого хода

--- CSharpEducation.TicTacToe/Program.cs
+++ CSharpEducation.TicTacToe/Program.cs
@@ -4,3 +4,13 @@ Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Добро пожаловать в игру крестики-нолики!");
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Выберите кто будет ходить первым: X и O");
+
+do
+{
+    selectedPlayer = Console.ReadKey(true).Key;
+
+} while (selectedPlayer != ConsoleKey.X && selectedPlayer != ConsoleKey.O);
+
+Console.ResetColor();
+Console.WriteLine($"Первым ходит: {selectedPlayer}");
+Console.ReadKey();
